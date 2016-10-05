1f6feaf6a3f93931c2d8788f33aa9bc1b1a04fc6
Jon Hall <jonathanphall@me.com>
2016-10-05 09:44:23 -0700

    update js file

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,16 @@ var cardOne = "queen"
 var cardTwo = "queen"
 var cardThree = "King"
 var cardFour = "King"
+
+if (cardOne === cardTwo) {
+	alert("You have a match!");
+}
+else if (cardOne === cardThree) {
+	alert("Sorry, try again.");
+}
+else if (cardThree === cardFour) {
+	alert("You have a match");
+}
+else if (cardThree === cardTwo) {
+	alert("Sorry not a match!");
+}
